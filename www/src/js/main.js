8c01ee6b7a2c829935111d14fa43fefe9bc8b842
Unknown <rickard_thomsen@hotmail.com>
2018-01-18 16:06:33 +0100

    js changes

--- www/src/js/main.js
+++ www/src/js/main.js
@@ -1,3 +1,6 @@
+import "../../../node_modules/popper.js/dist/umd/popper.js";
+import "../../../node_modules/bootstrap/dist/js/bootstrap.min.js";
+// import "../../../node_modules/jsonflex/jsonflex.js";
 import Test from './classes/test.class.js';
 
 let test = new Test();
