cc042cd305cb119f4759e4cde7581bef7dfafcbd
Unknown <rickard_thomsen@hotmail.com>
2018-01-19 13:43:22 +0100

    last fix on jquery import

--- www/src/js/main.js
+++ www/src/js/main.js
@@ -1,6 +1,13 @@
+import jQuery from "../../../node_modules/jquery/dist/jquery.js";
 import "../../../node_modules/popper.js/dist/umd/popper.js";
 import "../../../node_modules/bootstrap/dist/js/bootstrap.min.js";
-// import "../../../node_modules/jsonflex/jsonflex.js";
-import Test from './classes/test.class.js';
+import Test from "./classes/test.class.js";
 
+
+//jQuery 
+window.$ = window.jQuery = jQuery;
+
+//classes
 let test = new Test();
+
+$('body').css('background', 'red');
