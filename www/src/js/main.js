76e0f090ca59b918314bb02956da28bbe334a012
Unknown <rickard_thomsen@hotmail.com>
2018-01-19 14:18:01 +0100

    Merge branch 'feature/gulp' into develop

--- www/src/js/main.js
+++ www/src/js/main.js
@@ -1,9 +1,12 @@
 import jQuery from "../../../node_modules/jquery/dist/jquery.js";
 window.$ = window.jQuery = jQuery;
 require('jsrender')($);
+import render from "./misc/render.js";
 import "../../../node_modules/popper.js/dist/umd/popper.js";
 import "../../../node_modules/bootstrap/dist/js/bootstrap.min.js";
 import Test from "./classes/test.class.js";
+
+render();
 
 //classes
 let test = new Test();
