940de328eaf6a7a8bda376fc58ef41e51ce90315
Jonas Ahrens <jonas.ahrens@gmail.com>
2018-01-22 11:29:48 +0100

    Fix infinite loop / memory leak on import

--- www/src/js/main.js
+++ www/src/js/main.js
@@ -3,10 +3,11 @@ import '../../../node_modules/popper.js/dist/umd/popper.js';
 import '../../../node_modules/bootstrap/dist/js/bootstrap.min.js';
 import App from './classes/app.class.js';
 import viewsSetup from './misc/views-setup';
+
+// @ts-ignore
+require('jsrender')(jQuery);
 // @ts-ignore
 window.$ = window.jQuery = jQuery;
-// @ts-ignore
-require('jsrender')($);
 
 // classes
 let app = new App();
