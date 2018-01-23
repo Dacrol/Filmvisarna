f5f2e2cdc1f7749716c6ccc635c9b75e4acef925
Jonas A <jonas.ahrens@gmail.com>
2018-01-23 13:03:52 +0100

    Add binding popStates to views

--- www/src/js/classes/pop-state-handler.class.js
+++ www/src/js/classes/pop-state-handler.class.js
@@ -1,8 +1,14 @@
 class PopStateHandler {
   constructor () {
+    this.viewMethods = {};
     this.addEventHandler();
-    this.changePage();
+    // this.changePage();
     window.addEventListener('popstate', () => this.changePage());
+  }
+
+  bindViewToPopState (url, viewMethod) {
+    console.log(this.viewMethods)
+    Object.assign(this.viewMethods, {[url]: viewMethod});
   }
 
   addEventHandler () {
@@ -24,27 +30,8 @@ class PopStateHandler {
     $('header a').removeClass('active');
     $(`header a[href="${url}"]`).addClass('active');
 
-    let urls = {
-      '/': 'home',
-      '/current': 'aktuella',
-      '/salons': 'salonger'
-    };
-
-    let methodName = urls[url];
-    this[methodName]();
-  }
-
-  home () {
-    // $('main').empty();
-    // console.log('hem')
-  }
-  aktuella () {
-    // $('main').empty();
-    // console.log('aktuella')
-  }
-  salonger () {
-    // $('main').empty();
-    // console.log('salonger')
+    // let methodName = this.urls[url];
+    this.viewMethods[url]();
   }
 }
 
