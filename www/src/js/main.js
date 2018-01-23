6c0fccaa70722658d86aaef3825cac09a19f1186
Unknown <rickard_thomsen@hotmail.com>
2018-01-23 14:59:42 +0100

    started

--- www/src/js/main.js
+++ www/src/js/main.js
@@ -4,11 +4,16 @@ import '../../../node_modules/bootstrap/dist/js/bootstrap.min.js';
 import App from './classes/app.class.js';
 import viewsSetup from './helpers/views-setup';
 
+
+
+
 // @ts-ignore
 require('jsrender')(jQuery);
 // @ts-ignore
 window.$ = window.jQuery = jQuery;
-
+require("../../../node_modules/owl.carousel/dist/owl.carousel");
 // classes
 let app = new App();
+
+window.$.owlCarousel = window.$.fn.owlCarousel;
 viewsSetup(app);
