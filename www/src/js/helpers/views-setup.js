8bc8a6dacd9fd4b397096e9e275c049614784851
Jonas Ahrens <jonas.ahrens@gmail.com>
2018-01-25 19:41:52 +0100

    Add screenings view

--- www/src/js/helpers/views-setup.js
+++ www/src/js/helpers/views-setup.js
@@ -1,6 +1,13 @@
+import Renderer from '../classes/renderer-base.class';
 export default function viewsSetup (app) {
   // The first argument can be null if the selector already has the class pop
-  app.bindViewWithJSON('a#pills-home', 'home', '/', '/json/movie-data.json', 'movies');
+  app.bindViewWithJSON(
+    'a#pills-home',
+    'home',
+    '/',
+    '/json/movie-data.json',
+    'movies'
+  );
   app.bindView('a#pills-current', 'aktuellfilmer', '/current', {});
   app.bindViewWithJSON(
     'a#pills-salonger',
@@ -15,6 +22,34 @@ export default function viewsSetup (app) {
     'posterfilm',
     '/film',
     '/json/movie-data.json',
-    'movies');
-  // console.log('Bound views succesfully');
+    'movies'
+  );
+  app.bindView(
+    null,
+    'screenings',
+    '/visningar',
+    /** @param {Renderer} Renderer */ async (Renderer, pathParams) => {
+      let data = await Promise.all([
+        $.getJSON('/json/movie-data.json'),
+        $.getJSON('/json/screenings.json'),
+        $.getJSON('/json/salong.json')
+      ]);
+      let contextData = data[1].map(screening => {
+        return Object.assign(
+          screening,
+          {
+            movieData: data[0].filter(movie => {
+              return movie.title_sv === screening.movie;
+            })
+          },
+          {
+            salonData: data[2].filter(salon => {
+              return salon.id === screening.salon;
+            })
+          }
+        );
+      });
+      Renderer.renderView('screenings', { screenings: contextData });
+    }
+  );
 }
