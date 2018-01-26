c6b438b8a5446f65894087198a581145b4b72f3a
Unknown <rickard_thomsen@hotmail.com>
2018-01-26 09:13:23 +0100

    progress not working fully

--- www/src/js/main.js
+++ www/src/js/main.js
@@ -1,19 +1,66 @@
-import jQuery from '../../../node_modules/jquery/dist/jquery.js';
+// import jQuery from '../../../node_modules/jquery/dist/jquery.js';
 import '../../../node_modules/popper.js/dist/umd/popper.js';
 import '../../../node_modules/bootstrap/dist/js/bootstrap.min.js';
 import App from './classes/app.class.js';
 import viewsSetup from './helpers/views-setup';
 
-
+// global.jQuery = require("jquery");
+ var $ = require('jquery');
 
 
 // @ts-ignore
-require('jsrender')(jQuery);
+require('jsrender')($);
 // @ts-ignore
-window.$ = window.jQuery = jQuery;
+window.$ = window.jQuery = $;
+
+// console.log($,'fweew')
+
+
 require("../../../node_modules/owl.carousel/dist/owl.carousel");
 // classes
 let app = new App();
 
-window.$.owlCarousel = window.$.fn.owlCarousel;
+
 viewsSetup(app);
+$.owlCarousel = $.fn.owlCarousel;
+
+// setTimeout(function(){
+
+// $(document).ready(function() {
+  $('.owl-carousel').owlCarousel({
+    items: 1,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    URLhashListener: true,
+    startPosition: 'URLHash',
+    nav: true,
+    autoHeight: true,
+    video: true,
+    responsiveRefreshRate: 100,
+    onChange: function(prop) {
+      console.log(prop)
+      pauseYT();
+    }
+});
+
+// });
+
+// }, 3000);
+
+function pauseYT(){
+  let iframe = $('.owl-item').find('iframe');
+
+  let command = {
+    "event": "command",
+    "func": "pauseVideo"
+  };
+
+  iframe.each(function() {
+    console.log(this,'derr');
+    console.log(this.contentWindow,'derr');
+  this.contentWindow.postMessage(JSON.stringify(command), "*");
+  });
+}
+
+
+
