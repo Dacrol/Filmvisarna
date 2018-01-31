8e95c9d61307666cad3227b31a148d7284c20a11
Jonas Ahrens <jonas.ahrens@gmail.com>
2018-01-31 20:08:33 +0100

    Fix rebinding of event handlers on page change
    :(

--- www/src/js/classes/pop-state-handler.class.js
+++ www/src/js/classes/pop-state-handler.class.js
@@ -21,7 +21,7 @@ class PopStateHandler extends Base {
   addEventHandler () {
     let that = this;
     // $('a.pop').unbind('click');
-    $('a.pop').off('click', 'a.pop');
+    $(document).off('click', 'a.pop');
     $(document).on('click', 'a.pop', function (e) {
       let href = $(this).attr('href');
       history.pushState(null, null, href);
