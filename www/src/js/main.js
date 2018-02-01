d435ac7c1e3a707ceb5138cab97483bce84af3ab
Jonas Ahrens <jonas.ahrens@gmail.com>
2018-02-01 08:02:29 +0100

    Move Owl patch to new module & set showinfo = 1

--- www/src/js/main.js
+++ www/src/js/main.js
@@ -4,6 +4,7 @@ import '../../../node_modules/popper.js/dist/umd/popper.js';
 import '../../../node_modules/bootstrap/dist/js/bootstrap.min.js';
 import App from './classes/app.class.js';
 import viewsSetup from './helpers/views-setup';
+import patchOwlCarousel from './helpers/owl-patch';
 
 // @ts-ignore
 require('jsrender')(jQuery);
@@ -23,32 +24,5 @@ let app = new App();
 
 viewsSetup(app);
 
-patchOwlCarousel('&iv_load_policy=3&rel=0&showinfo=0&controls=1');
+patchOwlCarousel('&iv_load_policy=3&rel=0&showinfo=1&controls=1');
 
-/**
- * Patches the Owl Carousel play function by appending additional YouTube parameters
- *
- * @param {string} additionalParams Parameters to append to the generated iframes
- * @see {@link https://developers.google.com/youtube/player_parameters}
- */
-function patchOwlCarousel (additionalParams) {
-  const playFn =
-    // @ts-ignore
-    window.$.fn.owlCarousel.prototype.constructor.Constructor.Plugins.Video
-      .prototype.play;
-  // @ts-ignore
-  window.$.fn.owlCarousel.prototype.constructor.Constructor.Plugins.Video.prototype.play = function (
-    event
-  ) {
-    playFn.apply(this, arguments);
-    let src = $('.owl-video-frame')
-      .children()
-      .first()
-      .prop('src');
-    console.log(src);
-    $('.owl-video-frame')
-      .children()
-      .first()
-      .prop('src', src + additionalParams);
-  };
-}
