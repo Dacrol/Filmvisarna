036498a83d091e00b4c236264cdc69c4fa9dd462
Rickard <rickard_thomsen@hotmail.com>
2018-02-02 10:46:22 +0100

    Add property logInHandler to main app

--- www/src/js/classes/app.class.js
+++ www/src/js/classes/app.class.js
@@ -1,7 +1,12 @@
 // import PopStateHandler from './pop-state-handler.class.js';
 import Renderer from './renderer-base.class';
+import LogInHandler from '../classes/log-in-handler.class.js';
 
 class App extends Renderer {
+  constructor () {
+    super();
+    this.LogInHandler = new LogInHandler();
+  }
 }
 
 export default App;
