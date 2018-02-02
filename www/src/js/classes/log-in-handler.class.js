cdb2977f49826aeca2f6d2a6493897b551604dfe
Rickard <rickard_thomsen@hotmail.com>
2018-02-02 11:35:35 +0100

    Continue adding to logInHandler added template
    With my brother from another mother (Roger)

--- www/src/js/classes/log-in-handler.class.js
+++ www/src/js/classes/log-in-handler.class.js
@@ -4,15 +4,43 @@ export default class LogInHandler extends Base {
   constructor () {
     super();
     this.signedIn = false;
-
-
+    this.signInButton = $('#sign-in');
+    console.log(this.signInButton)
   }
 
+  // behöver skapa en login metod
+  // logIn() {
+
+  // }
 
 
+  template1 () {
+    return `
+      <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-body">
+              <form>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Email address</label>
+                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Password</label>
+                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+  }
 
+  // behöver skapa en loggga ut metod
 
+  // en metod som kollar om du är inloggad
+
+  // renedera ut formuläret
 }
-
-
-
