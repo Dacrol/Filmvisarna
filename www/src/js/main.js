8b73eb8f726cb527b00082a19da6be4ace509e02
Petter <petter.f.andersson@gmail.com>
2018-02-06 15:22:01 +0100

    WIP: Iterate out seats in salon

--- www/src/js/main.js
+++ www/src/js/main.js
@@ -26,3 +26,19 @@ viewsSetup(app);
 
 patchOwlCarousel('&iv_load_policy=3&rel=0&showinfo=1&controls=1');
 
+
+// Testfunktion för att iterera ut alla säten i salongen
+// JSON._load('/salong.json').then(function(salong){
+//   console.log(salong);
+// });
+
+// function renderSalon(){
+//   let row = $('<div>');
+//     for(let i = 0; i < 8; i++){
+//       let seats = $('<div>').addClass('seat');
+//       $(row).append(seats);
+//     }
+//     $('.salon-container').append(row);
+// }
+
+// renderSalon();
