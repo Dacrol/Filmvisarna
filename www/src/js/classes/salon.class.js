3b58feb982e92dbfb1c00cd0b1c23d42d8fd2f13
Petter <petter.f.andersson@gmail.com>
2018-02-07 15:09:55 +0100

    Added seatNr variable and changed append to prepend
    Detta för att ge varje säte ett unikt nummer, istället för att rad 1 hade haft platserna 1-8, rad 2 också hade haft plaserna 1-8 osv.

--- www/src/js/classes/salon.class.js
+++ www/src/js/classes/salon.class.js
@@ -6,9 +6,11 @@ class Salon extends App {
 		// Vilka parametrar behöver Salon-klassen?
 	}
 
+	// TODO: Lägg till parameter i renderSeats-metoden som väljer Stora eller Lilla salongen (just nu renderas bara Stora salongen (salonSeats[0]) ut)
 	async renderSeats(){
 		let salonSeats = await JSON._load('salong.json');
 
+		let seatNr = 0;
 		// Första for-loopen skriver ut antalet rader i salongen (baserat på length)
 		for(let i = 0; i < salonSeats[0].seatsPerRow.length; i++){
 			let row = $('<div>');
@@ -16,8 +18,9 @@ class Salon extends App {
 		  for(let j = salonSeats[0].seatsPerRow[i]; j > 0; j--){
 		  	// Varje individuellt säte får en unik koordinat med attributen data-rownumber och data-seatnumber i sitt element
 		  	// Dessa kan inspekteras i DOM:et
-				let col = $('<div>').addClass('seat').attr('data-rownumber', (i + 1)).attr('data-seatnumber', j);
-				$(row).append(col);
+		  	seatNr++;
+				let col = $('<div>').addClass('seat').attr('data-rownumber', (i + 1)).attr('data-seatnumber', seatNr);
+				$(row).prepend(col);
 				}
 			$('.salon-container').append(row);
 		}
