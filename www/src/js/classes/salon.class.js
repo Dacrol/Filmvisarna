1fb13737d96098c92eaaffca5d48d51f85b41d79
Petter <petter.f.andersson@gmail.com>
2018-02-07 11:20:05 +0100

    Begun creating a renderSeats-method

--- www/src/js/classes/salon.class.js
+++ www/src/js/classes/salon.class.js
@@ -1,12 +1,18 @@
 class Salon {
-	constuctor(){
+	constructor(){
 
 	}
 
 	renderSeats(){
 		JSON._load('salong.json').then(function(seats){
 			for(let seat of seats){
-				
+			  let row = $('<div>');
+			  for(let i = 0; i < 8; i++){
+			    let seats = $('<div>').addClass('seat');
+			    row.append(seats);
+			  }
+			  $('.salon-container').append(row);
+				}
 			}
 		}
 	}
