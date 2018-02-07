b336366fd9320055428a8d0c02e5f065d47233ec
Petter <petter.f.andersson@gmail.com>
2018-02-07 12:32:30 +0100

    Added another for-loop outside the two existing ones

--- www/src/js/classes/salon.class.js
+++ www/src/js/classes/salon.class.js
@@ -6,13 +6,15 @@ class Salon {
 	renderSeats(){
 		JSON._load('salong.json');
 
-	  for(let i = 0; i < {{:salons[0].seatsPerRow}}; i++){
-			let row = $('<div>');
-			for(let j = 0; j < {{:salons[0].seatsPerRow[i]}}; j++){
-				let col = $('<div>').addClass('seat');
-				$(row).append(col);
-			}
-			$('.salon-container').append(row);
+		for(let i = 0; i < {{:salons[0].seatsPerRow.length}}; i++){
+		  for(let j = 0; j < {{:salons[0].seatsPerRow[i]}}; j++){
+				let row = $('<div>');
+				for(let k = 0; k < {{:salons[0].seatsPerRow[j]}}; k++){
+					let col = $('<div>').addClass('seat');
+					$(row).append(col);
+				}
+				$('.salon-container').append(row);
+				}
 			}
 		}
 	}
