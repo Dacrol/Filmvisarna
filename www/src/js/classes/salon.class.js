13e5cf594cf9da0a313d144f67b29a03596487a4
Jonas Ahrens <jonas.ahrens@gmail.com>
2018-02-10 01:39:18 +0100

    Add hover on adjacent seats

--- www/src/js/classes/salon.class.js
+++ www/src/js/classes/salon.class.js
@@ -1,18 +1,21 @@
 import Base from './base.class.js';
 
 class Salon extends Base {
-  constructor () {
+  constructor (salongNr) {
     super();
     // Vilka properties behöver Salon-klassen?
-
+    this.salongNr = salongNr;
+    this.salonSeats = [];
     $(window).on('resize', () => this.scale());
     this.scale();
-    $(window).on('resize', function () {
-      let saloonHeight = $('.salon-wrapper').outerHeight();
-      console.log(saloonHeight);
+    $(window)
+      .on('resize', function () {
+        let saloonHeight = $('.salon-wrapper').outerHeight();
+        console.log(saloonHeight);
 
-      $('button.booking').css('margin-top', saloonHeight + 20);
-    }).trigger('resize');
+        $('button.booking').css('margin-top', saloonHeight + 20);
+      })
+      .trigger('resize');
   }
 
   scale () {
@@ -37,16 +40,23 @@ class Salon extends Base {
   // Parametern hos renderSeats() ska antingen vara 0 för Stora salongen eller 1 för Lilla salongen (se salong.json)
   // Just nu ges parametern värdet 0 när metoden anropas i view-setup.js
   // Detta måste ändras senare då man istället ska anropa salon.renderSeats() beroende på vilken salong filmen man vill se går i
-  async renderSeats (salongNr) {
-    // @ts-ignore
-    let salonSeats = await JSON._load('salong.json');
+  async renderSeats (salongNr = undefined) {
+    // Make sure this.salongNr matches the rendered salon for later use
+    if (typeof salongNr !== 'number') {
+      salongNr = this.salongNr;
+    } else {
+      this.salongNr = salongNr;
+    }
+
+    let allSalons = await JSON._load('salong.json');
+    this.salonSeats = allSalons[salongNr].seatsPerRow; // Save for later use in hover function
 
     let seatNr = 0;
     // Första for-loopen skriver ut antalet rader i salongen (baserat på length)
-    for (let i = 0; i < salonSeats[salongNr].seatsPerRow.length; i++) {
+    for (let i = 0; i < allSalons[salongNr].seatsPerRow.length; i++) {
       let row = $('<div>');
       // Andra for-loopen skriver ut sätena i respektive rad (adderar CSS-klassen 'seat')
-      for (let j = salonSeats[salongNr].seatsPerRow[i]; j > 0; j--) {
+      for (let j = allSalons[salongNr].seatsPerRow[i]; j > 0; j--) {
         // Varje individuellt säte får en unik koordinat med attributen data-rownumber och data-seatnumber i sitt element
         // Dessa kan inspekteras i DOM:et
         seatNr++;
@@ -60,35 +70,43 @@ class Salon extends Base {
     }
 
     this.amountOfSeats();
-
-    this.hoverSeat(
-      $('#number-of-visitors')
-        .val()
-        .toString()
-    );
     this.selectSeat();
   }
 
   amountOfSeats () {
     let that = this;
-    $('#number-of-visitors').on('change', function () {
-      let seats = $(this)
-        .val()
-        .toString();
-      that.hoverSeat(seats);
-    });
+    $('#number-of-visitors')
+      .on('change', function () {
+        let seats = $(this).val();
+        if (typeof seats === 'string') that.hoverSeat(parseInt(seats));
+      })
+      .trigger('change');
   }
 
-  hoverSeat (seats = '') {
+  hoverSeat (seats = 1) {
+    let that = this;
     // console.log(seats);
-    $('.seat').off('mouseover mouseleave');
-    $('.seat').on('mouseover', function () {
-      $(this).addClass('hovered');
-    });
-
-    $('.seat').on('mouseleave', function () {
-      $(this).removeClass('hovered');
-    });
+    $('.seat').off('mouseenter mouseleave');
+    $('.seat').hover(
+      function () {
+        let row = $(this).attr('data-rownumber');
+        let seat = $(this).attr('data-seatnumber');
+        // console.log(row, seat);
+        let seatNumbers = that.getAdjacent(+seat, seats, that.salonSeats, +row);
+        // console.log(seatNumbers);
+        $(this)
+          .siblings()
+          .addBack()
+          .filter(function () {
+            let seat = $(this).attr('data-seatnumber');
+            return seatNumbers.includes(+seat);
+          })
+          .addClass('hovered');
+      },
+      function () {
+        $('.seat').removeClass('hovered');
+      }
+    );
   }
 
   // kunna returnera hur många platser i salongen som en visning har
@@ -116,21 +134,20 @@ class Salon extends Base {
     const max = seatsPerRow.reduce((acc, seats, index) => {
       return index <= row ? acc + seats : acc;
     });
-    const min = max - seatsPerRow[row];
-    for (let count = 0; count < amount * 2; count++) {
+    const min = max - seatsPerRow[row] + 1;
+    for (let count = 0; count <= (amount - 1) * 2; count++) {
       let offset = count % 2 !== 0 ? (count + 1) / 2 : -(count / 2);
       let seat = start + offset;
       if (!(seat > max || seat < min)) {
         seatNumbers.push(seat);
         if (seatNumbers.length === amount) {
-          break;
+          return seatNumbers.sort((a, b) => {
+            return a - b;
+          });
         }
       }
     }
-    seatNumbers.sort((a, b) => {
-      return a - b;
-    });
-    return seatNumbers;
+    return [];
   }
 }
 
