ab1a817000f5e66c93f700099d8ecc947dbeb6cc
Jonas Ahrens <jonas.ahrens@gmail.com>
2018-02-12 01:24:34 +0100

    Separate tab contents

--- www/src/js/classes/log-in-handler.class.js
+++ www/src/js/classes/log-in-handler.class.js
@@ -34,8 +34,8 @@ export default class LogInHandler extends Base {
     // @ts-ignore
     let password = User.encrypt($('#password').val()).words.join();
     // @ts-ignore
-    this.allUserNames = await JSON._load('users.json');
-    for (let user of this.allUserNames) {
+    let allUserNames = await JSON._load('users.json');
+    for (let user of allUserNames) {
       if (user.id === userName) {
         if (user.password.words.join() === password) {
           this.confirmLogIn(user);
@@ -91,8 +91,8 @@ export default class LogInHandler extends Base {
   }
 
   registerUser () {
-    let userName = $('#email').val();
-    let password = $('#password').val();
+    let userName = $('#email-register').val();
+    let password = $('#password-register').val();
     let passwordMatch = $('#password-match').val();
 
     if (password === passwordMatch) {
@@ -103,45 +103,61 @@ export default class LogInHandler extends Base {
         })
         .catch(() => {
           console.log('Username taken');
-          $('#email').addClass(' is-invalid');
+          $('#email-register').addClass(' is-invalid');
           // console.log(error);
         });
     }
   }
 
   template1 () {
-    return `<div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    return `<div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="loginModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
 
-      <ul class="nav nav-tabs nav-fill">
+      <ul class="nav nav-tabs nav-fill" id="login-tabs" role="tablist">
         <li class="nav-item">
-          <a class="nav-link active" data-toggle="tab" href="#login">Logga in</a>
+          <a class="nav-link active" id="login-tab" data-toggle="tab" href="#login">Logga in</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" data-toggle="tab" href="#register">Registrera dig</a>
+          <a class="nav-link" id="register-tab" data-toggle="tab" href="#register">Registrera dig</a>
         </li>
       </ul>
-
-      <div class="modal-body">
-        <form id="test" action="post">
-          <div class="form-group">
-            <label for="email">E-post</label>
-            <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
+      <div class="tab-content" id="login-tabs-content">
+          <div class="tab-pane fade show active" id="login" role="tabpanel" aria-labelledby="login-tab">
+            <div class="modal-body">
+              <form id="login-form">
+                <div class="form-group">
+                  <label for="email">E-post</label>
+                  <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="E-post">
+                </div>
+                <div class="form-group">
+                  <label for="password">Lösenord</label>
+                  <input type="password" class="form-control" id="password" placeholder="Lösenord">
+                </div>
+                <button id="sign-in-submit" class="btn btn-primary" type="submit">Logga in</button>
+              </form>
+            </div>
           </div>
-          <div class="form-group">
-            <label for="password">Lösenord</label>
-            <input type="password" class="form-control" id="password" placeholder="Password">
+          <div class="tab-pane fade" id="register" role="tabpanel" aria-labelledby="register-tab">
+              <div class="modal-body">
+                  <form id="register-form">
+                    <div class="form-group">
+                      <label for="email-register">E-post</label>
+                      <input type="email" class="form-control" id="email-register" aria-describedby="emailHelp" placeholder="E-post">
+                    </div>
+                    <div class="form-group">
+                      <label for="password-register">Lösenord</label>
+                      <input type="password" class="form-control" id="password-register" placeholder="Lösenord">
+                    </div>
+                    <div class="form-group">
+                      <label for="password-match">Upprepa lösenord</label>
+                      <input type="password" class="form-control" id="password-match" placeholder="Upprepa lösenord">
+                    </div>
+                    <button id="register-user-submit" class="btn btn-primary" type="submit">Registrera dig</button>
+                  </form>
+                </div>
           </div>
-          <div class="form-group">
-            <label for="password-match">Lösenord</label>
-            <input type="password" class="form-control" id="password-match" placeholder="Password">
-          </div>
-          <a id="sign-in-submit" href="#" role="button" class="btn btn-primary">Submit</a>
-          <a id="register-user" href="#" role="button" class="btn btn-primary">Registrera dig</a>
-          <a id="register-user-submit" href="#" role="button" class="btn btn-primary">Submit reg</a>
-        </form>
-      </div>
+        </div>
     </div>
   </div>
 </div>`;
